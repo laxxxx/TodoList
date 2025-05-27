77c18380b438d368f08fd052317b19362adcb8e0
laxxxx <lakshmancool9@gmail.com>
2025-05-27 11:34:20 +0530

    initial commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Sree Lakshman on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
